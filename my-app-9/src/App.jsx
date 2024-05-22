770903b037df78b30fa031bad238769105d7c7cd
MrParth11 <124144809+MrParth11@users.noreply.github.com>
2024-05-22 18:39:01 +0530

    Add

---
+++ my-app-9/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Login from './component/Login'
+import Data from './component/Data';
+
+function App() {
+  
+
+  return (
+    <>
+      <Login />
+      {/* <Data/> */}
+    </>
+  )
+}
+
+export default App
